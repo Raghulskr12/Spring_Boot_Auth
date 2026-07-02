d5b437f055e1dce52af95f33c2c78fe51721ad37
Raghulskr12 <kalairaghul70@gmail.com>
2026-07-02 16:27:09 +0530

    [ADD] session id get added

--- auth/src/main/java/com/example/auth/controller/HelloController.java
+++ auth/src/main/java/com/example/auth/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.auth.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,9 @@ public class HelloController {
     {
         return "Hello World";
     }
+    @GetMapping("/sessionID")
+    public String sessionID(HttpServletRequest request)
+        {
+        return "sessionID" + request.getSession().getId();
+        }
 }
